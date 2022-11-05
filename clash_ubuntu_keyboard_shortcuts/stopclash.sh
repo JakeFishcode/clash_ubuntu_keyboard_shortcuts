6e53aeb2d337402257eec7fdbb34c919574a4e7a
Jakefishcode <wobudajianpan@qq.com>
2022-11-05 23:45:08 +0800

    change

--- clash_ubuntu_keyboard_shortcuts/stopclash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf /home/${LOGNAME}/.clash/tmp
-ps -xal | grep "/home/${LOGNAME}/.clash/clash -d /home/jk/.clash/." |awk '{print $3}' >> /home/${LOGNAME}/.clash/tmp
-read pid1 pid2 </home/${LOGNAME}/.clash/tmp
-kill -INT $pid1
-rm -rf /home/${LOGNAME}/.clash/tmp
-echo ${pid1}
-
